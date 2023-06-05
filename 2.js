f715c22a4c859290dfa7935e9c53dcf03b42fcab
vchandu111 <vchanduu111@gmail.com>
2023-06-05 10:13:34 +0530

    new data

--- 2.js
+++ 2.js
@@ -1,10 +1,47 @@
-var listOfItems = [
-  { brandName: "Nike", itemName: "shoe12", dateOfPurchase: "2012-04-22" },
-  { brandName: "Puma", itemName: "shoe04", dateOfPurchase: "2014-04-11" },
-  { brandName: "Nike", itemName: "jeans22", dateOfPurchase: "2019-11-25" },
-  { brandName: "Adidas", itemName: "shoe07", dateOfPurchase: "2013-06-27" },
-  { brandName: "Nike", itemName: "t-shirt15", dateOfPurchase: "2022-11-12" },
-  { brandName: "Adidas", itemName: "jacket16", dateOfPurchase: "2012-03-13" },
-];
-// For each particular brand-name, we have to fetch the item name, with the latest (Most recent) date of Purchase.
-// Print the result: ["shoe04", "shoe07", "t-shirt15"]
+function test(str, arr) {
+  let suffix = "";
+  let count = 1;
+
+  while (arr.includes(str + suffix)) {
+    suffix = `(${count})`; 
+    count++;
+  }
+
+  const modifiedString = str + suffix;
+  arr.push(modifiedString); 
+  return arr;
+}
+
+const myString = "c";
+const myArray = ["a", "b", "c", "d"];
+
+const modifiedArray = test(myString, myArray);
+console.log(modifiedArray);
+
+function appendArrayToArray(arr1, arr2) {
+  function appendStringWithSuffix(baseStr, arr) {
+    let suffix = "";
+    let count = 1;
+
+    while (arr.includes(baseStr + suffix)) {
+      suffix = `(${count})`;
+      count++;
+    }
+
+    return baseStr + suffix;
+  }
+
+  const modifiedArray = [];
+  arr1.forEach((str) => {
+    const modifiedString = appendStringWithSuffix(str, arr2);
+    modifiedArray.push(modifiedString);
+  });
+
+  return arr2.concat(modifiedArray);
+}
+
+const myArray1 = ["a", "b", "c"];
+const myArray2 = ["a", "e", "c"];
+
+const modifiedArray = appendArrayToArray(myArray1, myArray2);
+console.log(modifiedArray);
